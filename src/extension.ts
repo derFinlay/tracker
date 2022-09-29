a5226792fad1b1f7b0e1fff6e8bb4e1a19fb3432
derFinlay <ibimseinskatze@gmail.com>
2022-09-29 23:42:41 +0200

    first tracking event listeners

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	let count = 0;
+
+	let counters = {
+		created: 0,
+		changes: 0,
+		terminals: 0,
+		editorsOpend: 0,
+		deleted: 0,
+	};
+
 	console.log('Congratulations, your extension "tracker" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tracker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tracker!');
+	
+	vscode.window.onDidOpenTerminal((e) => {
+		counters.terminals++;
+		vscode.window.showInformationMessage(
+			'Terminals opend: ' + counters.terminals
+		);
+	});
+	vscode.window.onDidChangeActiveTextEditor((e) => {
+		counters.editorsOpend++;
+		vscode.window.showInformationMessage(
+			'Editors opend: ' + counters.editorsOpend
+		);
 	});
 
-	context.subscriptions.push(disposable);
+	vscode.workspace.onDidDeleteFiles((e) => {
+		counters.deleted += e.files.length;
+		vscode.window.showInformationMessage(
+			'Files deleted: ' + counters.deleted
+		);
+	});
+
+	vscode.workspace.onDidCreateFiles((e) => {
+		counters.created += e.files.length;
+		vscode.window.showInformationMessage(
+			'Files created: ' + counters.created
+		);
+	});
+
+	vscode.workspace.onDidChangeTextDocument((e) => {
+		if (e.contentChanges[0].text.length === 0) {
+			return;
+		}
+		counters.changes += e.contentChanges[0].text.length;
+		vscode.window.showInformationMessage('Changes: ' + counters.changes);
+	});
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
